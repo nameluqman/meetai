d5e17629b2b095c6d1f128090f092b522aff69b2
nameluqman <luqmanpoints090@gmail.com>
2025-11-23 10:55:19 +0500

    03: authentication setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,111 @@
-import {Button} from "@/components/ui/button"
+"use client";
+import { useState } from "react";
+
+import {Button} from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { auth } from "@/lib/auth";
+import { authClient } from "@/lib/auth-client";
+
 
 export default function Home() {
+const {data: session} = authClient.useSession() 
+
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = () => {
+    authClient.signUp.email({
+      name,
+      email,
+      password
+    },
+      {
+        onError: () => {
+          window.alert("Error: Something went wrong");
+      },
+      
+        onSuccess: () => {
+          window.alert("Success: Account created");
+        }
+      
+    });
+  }
+  const onLogin = () => {
+    authClient.signIn.email({
+      email,
+      password
+    },
+      {
+        onError: () => {
+          window.alert("Error: Something went wrong");
+      },
+      
+        onSuccess: () => {
+          window.alert("Success: Account created");
+        }
+      
+    });
+  }
+if (session){
   return (
-   <Button variant="outline">
-          click me
-    </Button>
+    <div className="p-4 flex flex-col gap-y-4">
+      <p>Welcome, {session.user.name}</p>
+
+      <Button onClick={()=>authClient.signOut()}>
+        Sign Out
+      </Button>
+    </div>
+    
+  )
+  };
+
+
+  return (
+    <div className="p-4 flex flex-col gap-y-p">
+    <div className="p-4 flex flex-col gap-y-4">
+   <Input 
+   placeholder="name" 
+   value={name} 
+   onChange={(e)=>setName(e.target.value)}
+   />
+
+   <Input 
+   placeholder="email" 
+   value={email} 
+   onChange={(e)=>setEmail(e.target.value)}
+   />
+
+   <Input 
+   placeholder="password" 
+   type="password" 
+   value={password} 
+   onChange={(e)=>setPassword(e.target.value)}
+   />
+   <Button onClick={onSubmit}>
+    Create Account
+   </Button>
+</div>  
+
+    <div className="p-4 flex flex-col gap-y-4">
+
+   <Input 
+   placeholder="email" 
+   value={email} 
+   onChange={(e)=>setEmail(e.target.value)}
+   />
+
+   <Input 
+   placeholder="password" 
+   type="password" 
+   value={password} 
+   onChange={(e)=>setPassword(e.target.value)}
+   />
+   <Button onClick={onLogin}>
+        Login in
+   </Button>
+</div>  
+</div>
   );
 }
---
+++ src/lib/auth-client.ts
@@ -0,0 +1,3 @@
+import { createAuthClient } from "better-auth/react"
+export const authClient = createAuthClient({});
+// we are on the same domain, so no need to specify apiUrl
