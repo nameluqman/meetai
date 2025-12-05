d6db6990b10941219beb0ef686fe8a11fe24914b
nameluqman <luqmanpoints090@gmail.com>
2025-12-05 15:58:20 +0500

    13: Agents Filters

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -1,46 +1,50 @@
 import { Suspense } from "react";
 import { headers } from "next/headers";
-import {redirect} from "next/navigation";
+import type { SearchParams } from "nuqs";
+import { redirect } from "next/navigation";
 
 import { ErrorBoundary } from "react-error-boundary";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { getQueryClient , trpc} from "@/trpc/server";
 import { auth } from "@/lib/auth";
-
-import { 
-    AgentsView, 
-    AgentsViewError, 
-    AgentsViewLoading 
-}  from "@/modules/agents/ui/views/agents-view";
+import { getQueryClient, trpc } from "@/trpc/server";
 import { AgentsListHeader } from "@/modules/agents/ui/components/agents-list-header";
+import { loadSearchParams } from "@/modules/agents/params";
 
+import {
+  AgentsView,
+  AgentsViewError,
+  AgentsViewLoading,
+} from "@/modules/agents/ui/views/agents-view";
 
+interface Props {
+  searchParams: Promise<SearchParams>;
+}
 
-const Page =async () =>{
+const Page = async ({ searchParams }: Props) => {
+  const filters = await loadSearchParams(searchParams);
 
-    const session = await auth.api.getSession({
-          headers: await headers(),
-        })
-        if(!session){
-          redirect('/sign-in');
-        }
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session) {
+    redirect("/sign-in");
+  }
 
-
-
-    const queryClient = getQueryClient();
-    void queryClient.prefetchQuery(trpc.agents.getmany.queryOptions());
-
-    return  ( 
-     <>
-     <AgentsListHeader />   
-    <HydrationBoundary state={dehydrate(queryClient)}>
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.agents.getmany.queryOptions({
+    ...filters,
+  }));
+  return (
+    <>
+      <AgentsListHeader />
+      <HydrationBoundary state={dehydrate(queryClient)}>
         <Suspense fallback={<AgentsViewLoading />}>
-        <ErrorBoundary fallback={<AgentsViewError />}>
+          <ErrorBoundary fallback={<AgentsViewError />}>
             <AgentsView />
-        </ErrorBoundary>    
+          </ErrorBoundary>
         </Suspense>
-    </HydrationBoundary>
+      </HydrationBoundary>
     </>
-    );
+  );
 };
 export default Page;
--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,52 +1,93 @@
 import z from "zod";
-import { eq, getTableColumns, sql } from "drizzle-orm";
+import {count,desc, and, eq, getTableColumns, ilike, sql } from "drizzle-orm";
 import { db } from "@/db";
-import { agents } from "@/db/schema";
+import { agents, user } from "@/db/schema";
 
-import { createTRPCRouter , protectedProcedure} from "@/trpc/init";
+import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { TRPCError } from "@trpc/server";
 import { agentsInsertSchema } from "../schemas";
 import { Input } from "@/components/ui/input";
+import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constants";
 
 export const agentsRouter = createTRPCRouter({
 
     //todo change "getOne" to "protectedProcedure"
-    getOne : protectedProcedure.input(z.object({id : z.string()})).query(async({input})=>{
+    getOne: protectedProcedure.input(z.object({ id: z.string() })).query(async ({ input }) => {
         const [existingAgent] = await db
-        .select({
-            meetingCount:sql<number>`5`,
-            ...getTableColumns(agents),
-    })
-        .from(agents)
-        .where(eq(agents.id,input.id))
-        
-        await new Promise ((resolve) => setTimeout(resolve,1000));
+            .select({
+                meetingCount: sql<number>`5`,
+                ...getTableColumns(agents),
+            })
+            .from(agents)
+            .where(eq(agents.id, input.id))
+
+        await new Promise((resolve) => setTimeout(resolve, 1000));
         // throw new TRPCError({ code : "BAD_REQUEST"});
-        
+
         return existingAgent;
     }),
     //todo change "getmany" to "protectedProcedure"
-    getmany : protectedProcedure.query(async()=>{
-        const data = await db
-        .select()
-        .from(agents)
-
-        await new Promise ((resolve) => setTimeout(resolve,1000));
-        // throw new TRPCError({ code : "BAD_REQUEST"});
-
-        return data;
-    }),
-    create:protectedProcedure
-    .input(agentsInsertSchema)
-    .mutation(async({input,ctx})=>{
-        const [createdAgent] = await db
-        .insert(agents)
-        .values({
-            ...input,
-            userId:ctx.auth.user.id,
+    getmany: protectedProcedure
+        .input(z.object({
+            page: z.number().default(DEFAULT_PAGE),
+            pageSize: z
+                .number()
+                .min(MIN_PAGE_SIZE)
+                .max(MAX_PAGE_SIZE)
+                .default(DEFAULT_PAGE_SIZE),
+            search: z.string().nullish()
         })
-        .returning();
+        )
+        .query(async ({ ctx, input }) => {
+            const {search ,page ,pageSize} = input;
+            const data = await db
+                .select({
+                    meetingCount: sql<number>`7`,
+                    ...getTableColumns(agents),
+                })
+                .from(agents)
+                .where(
+                    and(
+                        eq(agents.userId, ctx.auth.user.id),
+                           search ? ilike(agents.name, `%${search}%`) : undefined,
+                    )
+                )
+                .orderBy(desc(agents.createdAt) , desc(agents.id))
+                .limit(pageSize)
+                .offset((page-1) * pageSize)
 
-        return createdAgent;
-    })
+
+            const [total] = await db
+               .select({count : count() })
+               .from(agents)
+                .where(
+                    and(
+                        eq(agents.userId, ctx.auth.user.id),
+                           search ? ilike(agents.name, `%${search}%`) : undefined,
+                    )
+                );
+            const totalPages =Math.ceil(total.count/pageSize);
+           
+            // await new Promise((resolve) => setTimeout(resolve, 1000));
+            // // throw new TRPCError({ code : "BAD_REQUEST"});
+
+            return {
+                items:data,
+                total:total.count,
+                totalPages,
+            };
+        }),
+    create: protectedProcedure
+        .input(agentsInsertSchema)
+        .mutation(async ({ input, ctx }) => {
+            const [createdAgent] = await db
+                .insert(agents)
+                .values({
+                    ...input,
+                    userId: ctx.auth.user.id,
+                })
+                .returning();
+
+            return createdAgent;
+        })
 });
