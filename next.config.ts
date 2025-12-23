75bca2e8c4f85743377f538cad33bff679a34dd3
nameluqman <luqmanpoints090@gmail.com>
2025-12-23 15:18:12 +0500

    security: mandatory patch for React2Shell and Next.js 16.1.1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* In Next.js 16, linting and type-checking are no longer 
+    part of the 'next build' command by default. 
+    You do not need ignoreDuringBuilds here anymore.
+  */
+  reactStrictMode: true,
 };
 
 export default nextConfig;
+
+
+
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
